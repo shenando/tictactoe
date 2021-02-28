0cd979858f337a3b930672b63f96c617b4269ca8
Shenandoah Grace <shenandoah.grace@gmail.com>
2021-02-28 15:02:13 -0500

    add previous function to new class, may need to be reworked

--- js/board.js
+++ js/board.js
@@ -1,22 +1,27 @@
 const btns = document.querySelectorAll('.btn')
-let turn = 0
 
 class Board {
   constructor() {
-    //references to boxes in the DOM called 'squares' 
+    let playerScore, opponentScore = []
   }
-  //mark box method
+  markBox() {
+    let turn = 0
+
+    for (const btn of btns) {
+      btn.addEventListener('click', (e) => {
+        e.preventDefault()
+        
+        turn++
+        console.log(turn)
+    
+        btn.innerText = turn % 2 === 0 ? 'O' : 'X'
+      })
+    }
+  }
+
+  checkBox() {
+    
+  }
   //check boxes whether x or o
 }
 
-//mark box method
-for (const btn of btns) {
-  btn.addEventListener('click', (e) => {
-    e.preventDefault()
-    
-    turn++
-    console.log(turn)
-
-    btn.innerText = turn % 2 === 0 ? 'O' : 'X'
-  })
-}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 //three classes: board (anything that changes the board), game, player
-//we can pull from them and code here, i think?
+//we can pull from them and code here - this is our main code, where we put all the word we did in the objects together to make the game functional
 //split up the projects into issues on github
+
+let gameBoard = new Board()
+
+gameBoard.markBox()
+
+console.log(gameBoard.checkBox())
